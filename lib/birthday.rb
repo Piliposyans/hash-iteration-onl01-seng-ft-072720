f33b4f1fdd8287de8baa1d96dac5f68904f88c71
Yevgeniy Piliposyans <piliposyans@icloud.com>
2020-08-10 05:51:12 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,10 +11,8 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
-    birthday_kids.each do 
-      "Timmy" => 9,
-      "Sarah" => 6,
-      "Amanda" => 27
-    }
+    birthday_kids.each do |name, age|
+    if age <= 12
+      puts 
   end
 end 
