15374100c8188161adeafd46d236b16d338b470f
Yevgeniy Piliposyans <piliposyans@icloud.com>
2020-08-10 06:05:40 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,10 +11,9 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
-    birthday_kids.each { "Timmy" => 9,
-      "Sarah" => 6,
-      "Amanda" => 27}
-    if age <= 12
+    birthday_kids.each do |name, age|
+    if age < 12
       puts "Happy Birthday #{name}! You are now #{age} years old!"
+      
   end
 end 
